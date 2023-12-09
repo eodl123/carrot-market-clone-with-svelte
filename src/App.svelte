e6ae877e606a62f7711ba82f94ddb366e2ec2cb8
jongho Kim <31331701+eodl123@users.noreply.github.com>
2023-12-09 21:19:53 +0900

    market-clone-svelte-firebase
    스벨트 파이어베이스 실습

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import "./css/style.css";
+  import { user$ } from "./store";
+
+  let login = false;
+
+  const routes = {
+    "/": Main,
+    "/signup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+{#if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
---
+++ src/pages/Main.svelte
@@ -0,0 +1,87 @@
+<script>
+  import { onMount } from "svelte";
+  import Footer from "../components/Footer.svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+
+  const calcTime = (timestamp) => {
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minute > 0) return `${minute}분 전`;
+    else if (second > 0) return `${second}초 전`;
+    else return "방금전";
+  };
+
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+
+  let items = [];
+
+  const db = getDatabase();
+  const itemsRef = ref(db, "items/");
+
+  onMount(() =>
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      items = Object.values(data).reverse();
+    })
+  );
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/chart-bar.svg" alt="chart-bar" />
+      <img src="assets/wifi.svg" alt="wifi" />
+      <img src="assets/battery.svg" alt="battery" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>역삼1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/arrow-down.svg" alt="arrowdown" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" alt="search" />
+      <img src="assets/menu.svg" alt="menu" />
+      <img src="assets/alert.svg" alt="alert" />
+    </div>
+  </div>
+</header>
+
+<main>
+  {#each items as item}
+    <div class="item-list">
+      <div class="item-list__img">
+        <img src={item.imgUrl} alt={item.title} />
+      </div>
+      <div class="item-list__info">
+        <div class="item-list__info-title">{item.title}</div>
+        <div class="item-list__info-meta">
+          {item.place}{calcTime(item.insertAT)}
+        </div>
+        <div class="item-list__info-price">{item.price}</div>
+        <div>{item.description}</div>
+      </div>
+    </div>
+  {/each}
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<Footer location="home" />
+
+<div class="media-info-msg">화면 사이즈를 줄여주세요</div>
+
+<style>
+  .info-bar__time {
+    color: blue;
+    font-weight: bold;
+  }
+</style>
